71916b77674f9b50ad2012f770d0675ca01031f6
rokoperki <roko.perisic@gmail.com>
2026-06-23 13:38:44 +0200

    boot with conditions

--- boot_sect.asm
+++ boot_sect.asm
@@ -1,27 +1,24 @@
-;simple boot that demonstrates the stack  
+;simple boot that demonstrates conditions 
+; checks conditions and prints char 
+mov bx, 3
 
-mov ah, 0x0e    ;int 10/ah = 0eh -> scrolling BIOS routine 
+cmp bx, 4
+jle less_equal
+cmp bx, 40
+jl less
+mov al, "C"
+jmp end 
 
-mov bp, 0x8000 ; set base of stack little above boot sector 
-mov sp, bp 
+less_equal:
+  mov al, "A"
+  jmp end
+less:
+  mov al, "B"
 
-push 'A' ; push characters to stack, pushed as 16-bit values
-push 'B'
-push 'C'
+end:
+  mov ah, 0x0e
+  int 0x10
+  jmp $
 
-pop bx  ; pop to register b, then copy bl (8bit char)
-mov al, bl 
-int 0x10
-
-pop cx ; same as above but in reg cx 
-mov al, cl 
-int 0x10
-
-mov al, [0x7ffe] ; print first char at stack 0x8000 - 0x2 (C now i not pop A)
-
-int 0x10 
-
-jmp $
-
-times 510-($-$$) db 0 
-dw 0xaa55 
+  times 510-($-$$) db 0 
+  dw 0xaa55 
