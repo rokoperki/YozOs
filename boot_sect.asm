78005fd39c9998b1cc582dfaaf75296f6a7a5c23
rokoperki <roko.perisic@gmail.com>
2026-06-23 13:24:44 +0200

    boot with stack loading

--- boot_sect.asm
+++ boot_sect.asm
@@ -1,30 +1,27 @@
-;simple boot that demonstrates addressing
-[org 0x7c00] ; expected memmory location
+;simple boot that demonstrates the stack  
 
 mov ah, 0x0e    ;int 10/ah = 0eh -> scrolling BIOS routine 
 
-;first attempt
-mov al, the_secret  ; moves offset to print as character - wont work 
+mov bp, 0x8000 ; set base of stack little above boot sector 
+mov sp, bp 
+
+push 'A' ; push characters to stack, pushed as 16-bit values
+push 'B'
+push 'C'
+
+pop bx  ; pop to register b, then copy bl (8bit char)
+mov al, bl 
 int 0x10
 
-;second attempt
-mov al, [the_secret] ; moves char at that address to print - works if [org 0x7c00] is set 
+pop cx ; same as above but in reg cx 
+mov al, cl 
 int 0x10
 
-;third attempt
-mov bx, the_secret  ; works if org isnt set, if yes prints " "
-add bx, 0x7c00
-mov al, [bx]
-int 0x10
+mov al, [0x7ffe] ; print first char at stack 0x8000 - 0x2 (C now i not pop A)
 
-;fourt attempt
-mov al, [0x7c1d] ; works if bytecode isnt changed 
-int 0x10
+int 0x10 
 
 jmp $
 
-the_secret:
-  db "X"
-
 times 510-($-$$) db 0 
-dw 0xaa55
+dw 0xaa55 
