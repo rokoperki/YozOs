d95998ea41bd0ba56b67a6b9df2066586d9783a2
rokoperki <roko.perisic@gmail.com>
2026-06-23 17:27:48 +0200

    boot with print hex address

--- boot_sect.asm
+++ boot_sect.asm
@@ -1,25 +1,18 @@
-;simple boot that prints using custom function 
+;simple boot that prints hex address using custom function 
 
 [org 0x7c00]
 
-mov bx, HELLO_MSG
-call print_string
-
-mov bx, GOODBYE_MSG 
-call print_string
+mov dx, 0x1fa6
+call print_hex
 
 jmp $
 
 %include "print_string.asm"
 
-
-
 ;data 
-HELLO_MSG:
-  db 'Hello World!  ', 0 
+HEX_OUT:
+  db '0x0000', 0 
 
-GOODBYE_MSG :
-  db 'Goodbye!', 0
 
 times 510-($-$$) db 0 
 dw 0xaa55 
--- print_string.asm
+++ print_string.asm
@@ -9,3 +9,27 @@ print_string:
   jmp start
   end:
     ret
+
+print_hex:
+  mov cx, 0 ; digit counter 
+
+  start_hex:
+  mov ax, dx
+  and ax, 0x000f
+  add al, '0'        ; assume it's a digit
+  cmp al, '9'        ; did we overshoot past '9'
+  jle done           ; no -> it was 0..9, leave it
+  add al, 7          ; yes -> it was A..F, jump the gap
+  done:
+    mov bx, HEX_OUT + 5 ; load last digit address  
+    sub bx, cx ; sub current digit 
+    mov [bx], al ; save parsed dx digit 
+  add cx, 1  ;increment 
+  shr dx, 4  ;shift dx for byte 0x1fb6 -> 0x01fb 
+  cmp cx, 4  ;jumo if last digit done
+  jl start_hex
+
+
+    mov bx, HEX_OUT
+    call print_string 
+    ret
