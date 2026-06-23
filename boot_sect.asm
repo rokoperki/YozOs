7d18e5b04d76cffd020406083bd34d6ba0084b63
rokoperki <roko.perisic@gmail.com>
2026-06-23 12:55:09 +0200

    simple boot with boot addressing

--- boot_sect.asm
+++ boot_sect.asm
@@ -1,23 +1,30 @@
-;simple boot sector that prints message using BUIS routine
+;simple boot that demonstrates addressing
+[org 0x7c00] ; expected memmory location
 
+mov ah, 0x0e    ;int 10/ah = 0eh -> scrolling BIOS routine 
 
-mov ah, 0x0e  ; int 10 -> scrolling teletype BIOS routine
-
-mov al, 'H'
-int 0x10
-mov al, 'e'
-int 0x10
-mov al, 'l'
-int 0x10
-mov al, 'l'
-int 0x10
-mov al, 'o'
+;first attempt
+mov al, the_secret  ; moves offset to print as character - wont work 
 int 0x10
 
-jmp $ ; infinite loop
+;second attempt
+mov al, [the_secret] ; moves char at that address to print - works if [org 0x7c00] is set 
+int 0x10
 
-;padding and BIOS num 
+;third attempt
+mov bx, the_secret  ; works if org isnt set, if yes prints " "
+add bx, 0x7c00
+mov al, [bx]
+int 0x10
+
+;fourt attempt
+mov al, [0x7c1d] ; works if bytecode isnt changed 
+int 0x10
+
+jmp $
+
+the_secret:
+  db "X"
 
 times 510-($-$$) db 0 
-
 dw 0xaa55
