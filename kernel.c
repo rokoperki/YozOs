bc283e9a1c28e537d675790013e7257efc7eda04
rokoperki <roko.perisic@gmail.com>
2026-06-30 13:13:33 +0200

    frame allocator test

--- kernel.c
+++ kernel.c
@@ -1,9 +1,10 @@
 #include "cpu/isr.h"
-#include "cpu/memory_map.h"
 #include "cpu/timer.h"
 #include "drivers/keyboard.h"
 #include "drivers/screen.h"
 #include "kernel/string.h"
+#include "memory/frame_alloc.h"
+#include "memory/memory_map.h"
 
 int main() {
   clear_screen();
@@ -43,5 +44,16 @@ void user_input(char *input) {
     asm volatile("hlt");
   } else if (strcmp(input, "MMAP") == 0) {
     memory_map_print();
+  } else if (strcmp(input, "FALLOC") == 0) {
+    init_frames();
+    char buf[50];
+    for (int i = 0; i < 4; i++) {
+      u32 addr = alloc_frame();
+      print_u64(addr, buf);
+      if (i == 2) {
+        free_frame(addr);
+      }
+      println("");
+    }
   }
 }
